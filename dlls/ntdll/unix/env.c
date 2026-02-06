thjjfyu/wineu

ntdll: workaround for locales on Android

dlls/ntdll/unix/env.c
@@ -805,7 +805,25 @@ static void init_locale(void)
     const NLS_LOCALE_HEADER *locale_table;
     const NLS_LOCALE_DATA *locale;
     char *p;
+#ifdef __ANDROID__
+    all = getenv( "LC_ALL" );
+    if (!all)
+        all = "C.UTF-8";
 
+    if (!unix_to_win_locale( all, system_locale )) system_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_ALL is %s, setting system locale to %s\n", debugstr_a(all), debugstr_a(system_locale) );
+
+    if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
+    {
+        FIXME( "HACK setting EN locale.\n" );
+        all = "en_US.UTF-8";
+    }
+        
+    if (!unix_to_win_locale( all, user_locale )) user_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_ALL is %s, user system locale to %s\n", debugstr_a(all), debugstr_a(user_locale) );
+#else
+    if (!unix_to_win_locale ( lc_all, user_locale )) user_locale[0] = 0;     
+    
     if (!(all = setlocale( LC_ALL, "" )) && (all = getenv( "LC_ALL" )))
         FIXME_(nls)( "Failed to set LC_ALL to %s, is the locale supported?\n", debugstr_a(all) );
     if (!(ctype = setlocale( LC_CTYPE, "" )) && (ctype = getenv( "LC_CTYPE" )))
@@ -824,7 +842,7 @@ static void init_locale(void)
 
     if (!unix_to_win_locale( messages, user_locale )) user_locale[0] = 0;
     TRACE_(nls)( "Unix LC_MESSAGES is %s, user system locale to %s\n", debugstr_a(messages), debugstr_a(user_locale) );
-
+#endif
 #ifdef __APPLE__
     if (!system_locale[0])
     {

bags432/wine_proton

dlls/ntdll/unix/env.c
@@ -809,7 +809,25 @@ static void init_locale(void)
     const NLS_LOCALE_HEADER *locale_table;
     const NLS_LOCALE_DATA *locale;
     char *p;
+#ifdef __ANDROID__
+    all = getenv( "LC_ALL" );
+    if (!all)
+        all = "C.UTF-8";
 
+    if (!unix_to_win_locale( all, system_locale )) system_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_ALL is %s, setting system locale to %s\n", debugstr_a(all), debugstr_a(system_locale) );
+
+    if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
+    {
+        FIXME( "HACK setting EN locale.\n" );
+        all = "en_US.UTF-8";
+    }
+        
+    if (!unix_to_win_locale( all, user_locale )) user_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_ALL is %s, user system locale to %s\n", debugstr_a(all), debugstr_a(user_locale) );
+#else
+    if (!unix_to_win_locale ( all, user_locale )) user_locale[0] = 0;     
+    
     if (!(all = setlocale( LC_ALL, "" )) && (all = getenv( "LC_ALL" )))
         FIXME_(nls)( "Failed to set LC_ALL to %s, is the locale supported?\n", debugstr_a(all) );
     if (!(ctype = setlocale( LC_CTYPE, "" )) && (ctype = getenv( "LC_CTYPE" )))
@@ -828,7 +846,7 @@ static void init_locale(void)
 
     if (!unix_to_win_locale( messages, user_locale )) user_locale[0] = 0;
     TRACE_(nls)( "Unix LC_MESSAGES is %s, user system locale to %s\n", debugstr_a(messages), debugstr_a(user_locale) );
-
+#endif
 #ifdef __APPLE__
     if (!system_locale[0])
     {